25202e9c0581fa66257b9dba79a58c60bd8569fd
Uilian Ries <uilianries@gmail.com>
2017-05-05 15:59:17 -0300

    #1 Increment patch version 0.1.1
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.1.0",
+    version="0.1.1",
 
     description='LDAP authenticator for Conan C/C++ package manager',
 
@@ -91,5 +91,5 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('%s/.conan_server/plugins/authenticatior' % expanduser('~'), ['conan/ldap_authentication.py'])],
+    data_files=[('%s/.conan_server/plugins/authenticator' % expanduser('~'), ['conan/ldap_authentication.py'])],
 )
